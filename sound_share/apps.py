e1a5b94d5a09b72961af66df8e08c6ee60b346c9
David Soards <davidysoards@gmail.com>
2020-10-28 03:30:13 -0400

    first commit

---
+++ sound_share/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SoundShareConfig(AppConfig):
+    name = 'sound_share'
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/*
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  experimental: {
+    applyComplexClasses: true,
+  },
+  purge: {
+    // enabled: true,
+    content: ['./**/*.html'],
+  },
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      screens: {
+        '2xl': '1560px',
+      },
+    },
+    variants: {},
+    plugins: [],
+  },
+};
