6c33501728edc87fa64a086a249659bde9921979
David Soards <davidysoards@gmail.com>
2020-10-29 21:59:52 -0400

    Add packs model

--- sound_share/models.py
+++ sound_share/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Pack(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, null=True, on_delete=SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+# class Pack(models.Model):
+
+#     title = models.CharField(_("title"), max_length=50)
+
+#     class Meta:
+#         verbose_name = _("pack")
+#         verbose_name_plural = _("packs")
+
+#     def __str__(self):
+#         return self.name
+
+#     def get_absolute_url(self):
+#         return reverse("pack_detail", kwargs={"pk": self.pk})
--- sound_share/views.py
+++ sound_share/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Pack
+
+# packs = [
+#     {
+#         "title": "Pack 1",
+#         "description": "An awesome pack of sounds.",
+#         "date_posted": "October 29, 2020",
+#         "author": "davidsoards",
+#     },
+#     {
+#         "title": "Pack 2",
+#         "description": "An amazing pack of sounds.",
+#         "date_posted": "October 29, 2020",
+#         "author": "davidsoards",
+#     },
+# ]
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    context = {"packs": Pack.objects.all()}
+    return render(request, "home.html", context)
 
 
 def about(request):
