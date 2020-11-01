cce85bb3fe6fdf887c0e733427b68bf991691f39
David Soards <davidysoards@gmail.com>
2020-11-01 01:46:13 -0500

    add profile page and profile update forms

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+# remove the default colon suffix from labels
+class BaseModelForm(forms.ModelForm):
+    required_css_class = "required"
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("label_suffix", "")
+        super(BaseModelForm, self).__init__(*args, **kwargs)
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -14,3 +23,17 @@ class UserRegistrationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("label_suffix", "")
         super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+
+class UserUpdateForm(BaseModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="avatar-default.svg", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+    def save(self):
+        super().save()
+
+        # TODO delete old profile images
+        img = Image.open(self.image.path)
+        if img.height > 512 or img.width > 512:
+            output_size = (512, 512)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
